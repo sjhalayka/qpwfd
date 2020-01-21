f8c4d17b30e7699f4d70398b7f953436dcce4dcc
Shawn Halayka <sjhalayka@gmail.com>
2020-01-21 15:18:08 -0600

    Add files via upload

--- ch1-3.cpp
+++ ch1-3.cpp
@@ -12,44 +12,7 @@ using namespace qpp;
 
 int main(void)
 {
-	// allocate enough space to hold the count of the number of ways of rolling
-	// a two through twelve inclusive, on a pair of six-sided dice
-	// d stands for dimension
 	size_t d = 11;
-
-	// Create bra
-	bra b(d);
-
-	// Set bra values
-	b << cd(1.0, 0.0), // there is only 1 way to roll a total of two
-		cd(2.0, 0.0), // three
-		cd(3.0, 0.0), // four
-		cd(4.0, 0.0), // five
-		cd(5.0, 0.0), // six
-		cd(6.0, 0.0), // seven
-		cd(5.0, 0.0), // eight
-		cd(4.0, 0.0), // nine
-		cd(3.0, 0.0), // ten
-		cd(2.0, 0.0), // eleven
-		cd(1.0, 0.0); // twelve
-
-	// Get sum
-	cd s;
-
-	for (size_t i = 0; i < d; i++)
-		s += b[i];
-
-	double sum = abs(s);
-
-	// Convert probabilities to probability amplitudes, normalize
-	for (size_t i = 0; i < d; i++)
-		b[i] = sqrt(b[i] / sum);
-
-	// Create ket
-	ket k(d);
-
-	// Automatically assign values
-	k = b;
 
 	// Create operator matrix
 	cmat A(d, d);
