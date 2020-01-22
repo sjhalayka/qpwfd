6aee125fc97e000c62ea84ac4503667f635b6591
Shawn Halayka <sjhalayka@gmail.com>
2020-01-21 21:30:29 -0600

    Add files via upload

---
+++ ch1-q14.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <complex>
+#include <cmath>
+using namespace std;
+
+#include "qpp.h"
+using namespace qpp;
+
+#define cd complex<double>
+
+
+int main(void)
+{
+	size_t d = 2;
+
+	// Create operator matrix
+	cmat A(d, d);
+
+	A << cd(3.0, 0.0), cd(-1.0, 0.0),
+		cd(4.0, 0.0), cd(-2.0, 0.0);
+
+	pair<dyn_col_vect<cd>, cmat> p = eig(A);
+
+	// Manually print eigenvalues and normalized eigenvectors
+	for (size_t i = 0; i < d; i++)
+		cout << disp(p.first(i)) << endl;
+
+	for (size_t i = 0; i < d; i++)
+		cout << disp(p.second.col(i)) << endl << endl;
+
+	return 0;
+}
