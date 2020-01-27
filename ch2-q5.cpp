becf87261bb19a26f0a5dbc2e045591c814c55ae
Shawn Halayka <sjhalayka@gmail.com>
2020-01-27 17:12:06 -0600

    Add files via upload

---
+++ ch2-q5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <complex>
+#include <cmath>
+using namespace std;
+
+#include "qpp.h"
+using namespace qpp;
+
+// abbreviate some syntax
+#define cd complex<double>
+
+
+int main(void)
+{
+	// See: https://www.wolframalpha.com/input/?i=integrate+sin%5E2%28pi+*+x+%2F+a%29dx+from+x%3D0+to+a
+
+	return 0;
+}
