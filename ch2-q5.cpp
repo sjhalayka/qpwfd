5dc0ff5d30e435eb3acc87d63f7e43cb4344e312
Shawn Halayka <sjhalayka@gmail.com>
2020-01-27 22:59:57 -0600

    Add files via upload

--- ch2-q5.cpp
+++ ch2-q5.cpp
@@ -13,6 +13,8 @@ using namespace qpp;
 int main(void)
 {
 	// See: https://www.wolframalpha.com/input/?i=integrate+sin%5E2%28pi+*+x+%2F+a%29dx+from+x%3D0+to+a
+	//
+	// integral_0^a sin^2((pi*x)/a) dx = a/2
 
 	return 0;
 }
